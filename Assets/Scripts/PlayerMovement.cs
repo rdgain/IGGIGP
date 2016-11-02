0a8fd60c7ce2147bed4ddc7cd7a26b39be78ae8a
Adam Sattaur <asatt014@cgs-macd-16950.campus.goldsmiths.ac.uk>
2016-11-02 12:51:46 +0000

    Initial penguin movement.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 0.2f;
+	public float maxVelocity = 10f;
+
+	private float LEFT = 0f;
+	private float RIGHT = 180f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		if (GetComponent<Rigidbody2D> ().velocity.magnitude < maxVelocity) {
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (x * speed, y * speed));
+		}
+
+	}
+}
